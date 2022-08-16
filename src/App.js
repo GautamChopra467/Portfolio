14c2e2f79496825328ce3e7c0b9597a681cca365
GautamChopra467 <harshchopra467@gmail.com>
2022-08-15 22:09:41 -0700

    Sixth commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Home from './routes/Home';
 import About from './routes/About';
 import Contact from './routes/Contact';
 import Project from './routes/Project';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
     <Routes>
-      <Route exact path={process.env.PUBLIC_URL + '/'} element={<Home />} />
+      <Route exact path={process.env.PUBLIC_URL} element={<Home />} />
       <Route path='/about' element={<About />} />
       <Route path='/project' element={<Project />} />
       <Route path='/contact' element={<Contact />} />
